5bddc72a23d87eb85bd891ae3e45f98889619eef
Julian J. Gonzalez <julian.gonzalez@seguridadparatodos.es>
2016-11-05 15:20:04 +0100

    Create efile1.py

---
+++ M4-S0.2/efile1.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+"""
+2017
+Máster Seguriad TIC
+M4 - Módulo Hacking & Pentestings 
+
+www.st2labs.com | @ST2Labs | @rhodius | @seguridadxato2
+
+"""
+
+# Definición de las tareas a ejecutar en la función principal
+def main():
+    # En python la tabulacion es fundamental (cada bloque de codigo)
+    # debe estar dentro del mismo identado (4 espacios cada vez)
+    
+    # Primer nivel de identado (4 espacios en blanco)
+        # Segundo nivel de identado (8 espacios en blanco)
+    print ' Hola world!'
+
+
+if __name__ == '__main__':
+    #Llamada a la función principal
+    main()
